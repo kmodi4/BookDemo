52ce1d79f3849fa76478e3256b190fc33c0257e2
KARAN <kmodi4@gmail.com>
2016-01-26 09:35:07 +0530

    new Update

---
+++ app/src/main/java/com/example/karan/bookdemo/GCMBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.example.karan.bookdemo;
+
+/**
+ * Created by KARAN on 26-01-2016.
+ */
+public class GCMBroadcastReceiver {
+}
---
+++ app/src/main/java/com/example/karan/bookdemo/splash.java
@@ -0,0 +1,52 @@
+package com.example.karan.bookdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(splash.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
