6f7d647f72f9b1e09ee2fcd58c391501491dfc80
KARAN <kmodi4@gmail.com>
2016-01-26 09:49:45 +0530

    new Update1

--- app/src/main/java/com/example/karan/bookdemo/MainActivity.java
+++ app/src/main/java/com/example/karan/bookdemo/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.karan.bookdemo;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1= (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registrationIntent = new Intent("com.google.android.c2dm.intent.REGISTRATION");
+                registrationIntent.putExtra("app", PendingIntent.getBroadcast(view.getContext(),0,new Intent(),0));
+                registrationIntent.putExtra("sender","642509541734");
+                startService(registrationIntent);
+            }
+        });
     }
 
     @Override
