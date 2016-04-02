f28b485cc4dd6c9fc15f16b12e14c9cd226cfcbb
KARAN <kmodi4@gmail.com>
2016-04-02 15:41:38 +0530

    LoginUpdate

--- app/src/main/java/com/example/karan/bookdemo/MainActivity.java
+++ app/src/main/java/com/example/karan/bookdemo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.karan.bookdemo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -22,6 +24,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +39,12 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
 
     private ViewFlipper mViewFlipper;
     private GestureDetector mGestureDetector;
+    private  NavigationView navigationView;
+    private SharedPreferences sharedPreferences;
+
+    private SharedPreferences.Editor editor;
+    private static Boolean status = false;
+
 
     int[] resources = {
             R.drawable.img1, R.drawable.img2,
@@ -59,12 +72,16 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
         t2 = (TextView) findViewById(R.id.textView6);
         hn = (TextView) findViewById(R.id.headername1);
         he= (TextView) findViewById(R.id.headeremail1);
+        sharedPreferences = getSharedPreferences("Login", Context.MODE_PRIVATE);
+        status = sharedPreferences.getBoolean("LStatus",false);
         //hn.setText("hey");
 
         b = getIntent().getExtras();
 
 
         mViewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
+
 
 
         // Add all the images to the ViewFlipper
@@ -119,12 +136,12 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         if(b!=null){
             String sbu1="";
-            String nm = "";
+            String nm;
             if(b.containsKey("name")) {
                 nm = b.getString("name");
                 //Toast.makeText(getApplicationContext(),nm,Toast.LENGTH_SHORT).show();
@@ -137,14 +154,21 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
                 Toast.makeText(getApplicationContext(), sbu1, Toast.LENGTH_SHORT).show();
 //                hn.setText(sbu1);
                 //he.setText(nm);
+                if(he!=null && hn!=null){
+                    Toast.makeText(getApplicationContext(),"Null Objects", Toast.LENGTH_SHORT).show();
+                }
+                newMenu();
             }
-            navigationView.getMenu().clear();
-            navigationView.inflateMenu(R.menu.loginmenu);
+
             /*Menu drawerMenu = navigationView.getMenu();
             for (int i = 1; i <= 3; i++) {
                 drawerMenu.add("Runtime item "+ i);
             }*/
 
+        }
+
+        if(status){
+            newMenu();
         }
 
 
@@ -180,6 +204,29 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        status = sharedPreferences.getBoolean("LStatus",false);
+        Log.e("ResumeStatus:", String.valueOf(status));
+        if(status){
+            newMenu();
+        }
+        else {
+            oldmenu();
+        }
+    }
+
+    public void newMenu(){
+        navigationView.getMenu().clear();
+        navigationView.inflateMenu(R.menu.loginmenu);
+    }
+
+    public void oldmenu(){
+        navigationView.getMenu().clear();
+        navigationView.inflateMenu(R.menu.activity_main_drawer);
+    }
+
     public static List<listinfo> getData(){
         List<listinfo> data = new ArrayList<>();
         int[] icon = {R.drawable.b1,
@@ -201,6 +248,18 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
 
     }
 
+    @Override
+    public void onNewIntent(Intent intent) {
+       status = sharedPreferences.getBoolean("LStatus",false);
+        Log.e("Status:",String.valueOf(status));
+        if(status){
+            newMenu();
+        }
+        else {
+            oldmenu();
+        }
+    }
+
 
 
     @Override
@@ -212,6 +271,8 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
             super.onBackPressed();
         }
     }
+
+
 
     class NavMenuClass{
         Menu menu;
@@ -254,6 +315,21 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
           else if (id == R.id.sell_book){
             Intent i = new Intent(MainActivity.this,sellbook.class);
             startActivity(i);
+        }
+        else if (id == R.id.lo){
+            Boolean gl = sharedPreferences.getBoolean("g+",false);
+            if(status){
+                //new login().signOut();
+                editor = sharedPreferences.edit();
+               // editor.putBoolean("g+",false);
+                editor.putBoolean("LStatus",false);
+                editor.apply();
+                oldmenu();
+            }
+
+
+
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -321,8 +397,25 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
 
             case R.id.login:
                 //Toast.makeText(getBaseContext(),"coming soon feature",Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(MainActivity.this,login.class);
-                startActivity(i);
+                status = sharedPreferences.getBoolean("LStatus",false);
+                if(status){
+                    Toast.makeText(getBaseContext(),"Already Logged in",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    editor = sharedPreferences.edit();
+                    if(sharedPreferences.getBoolean("g+",false)){
+                        editor.putBoolean("gout", true);
+                        editor.putBoolean("g+",false);
+                    }
+                    else {
+                        editor.putBoolean("gout",false);
+                    }
+                    Log.e("loging+",String.valueOf(sharedPreferences.getBoolean("g+",false)));
+
+                    editor.apply();
+                    Intent i = new Intent(MainActivity.this, login.class);
+                    startActivity(i);
+                }
                 //finish();
                 return true;
         }
